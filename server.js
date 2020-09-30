cbac7cb3ef2c8f4102373ac342127e25e2e30d68
Magdalena Świderska <m.swiderska@outlook.com>
2020-09-30 11:57:51 +0200

    Add middleware express.static public folder

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/', (req, res) => {
   res.show('index.html');
 });
@@ -28,15 +30,6 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.show('history.html');
-});
-
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/style.css'));
-});
-
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/test.png'));
 });
 
 
