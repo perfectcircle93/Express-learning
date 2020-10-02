7b353fa6e87d8686f6f6ceac1f420452046e86ad
Magdalena Świderska <m.swiderska@outlook.com>
2020-10-02 18:29:01 +0200

    Add layout and dark.hbs

--- server.js
+++ server.js
@@ -3,33 +3,33 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.engine('hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.render('index', {layout: false});
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', {layout: false});
+  res.render('about.hbs', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {layout: false});
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', {layout: false});
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', {layout: false});
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello');
 });
 
 
